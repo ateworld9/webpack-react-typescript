d1ee46d4b1e3721c57eecd5259805a873a96bb68
Dmitriy <vahrameev.work@gmail.com>
2022-11-15 12:11:40 +0300

    react 18.2.0 +ts webpack working config, need to test

---
+++ webpack/config/devServer.js
@@ -0,0 +1,20 @@
+/**
+ * @see https://webpack.js.org/configuration/dev-server/
+ */
+
+ import {devServerProxyConfig} from './devServerProxy';
+
+ export const devServerConfig = {
+     client: {
+         overlay: false,
+     },
+     headers: {'Access-Control-Allow-Origin': '*'},
+     historyApiFallback: true,
+     hot: true,
+     proxy: devServerProxyConfig,
+     static: {
+         publicPath: '/',
+     },
+     port: 3000
+ };
+ 
